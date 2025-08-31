760088ab5a7f19683d9a67cb80ffb0593784ed8a
Rizwan <rizwanmushtaq15@gmail.com>
2025-08-31 21:35:31 +0200

    added two sum fast code and unit tests

---
+++ src/main/java/com/rizwanmushtaq/ch1section4/TwoSumFast.java
@@ -0,0 +1,17 @@
+package com.rizwanmushtaq.ch1section4;
+
+import java.util.Arrays;
+
+public class TwoSumFast {
+
+  public int count(int[] a) {
+    int n = a.length;
+    int count = 0;
+    Arrays.sort(a);
+    for (int i = 0; i < n; i++) {
+      int j = Arrays.binarySearch(a, -a[i]);
+      if (j > i) count++;
+    }
+    return count;
+  }
+}
