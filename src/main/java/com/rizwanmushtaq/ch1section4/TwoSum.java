c1553cb71fddec3bfefbbe50d331d72b56ef82b3
Rizwan <rizwanmushtaq15@gmail.com>
2025-08-31 21:26:07 +0200

    added brute force two sum code and unit tests

---
+++ src/main/java/com/rizwanmushtaq/ch1section4/TwoSum.java
@@ -0,0 +1,17 @@
+package com.rizwanmushtaq.ch1section4;
+
+public class TwoSum {
+
+  public int count(int[] a) {
+    int n = a.length;
+    int count = 0;
+    for (int i = 0; i < n; i++) {
+      for (int j = i + 1; j < n; j++) {
+        if (a[i] + a[j] == 0) {
+          count++;
+        }
+      }
+    }
+    return count;
+  }
+}
