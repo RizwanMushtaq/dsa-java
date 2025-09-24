ebb8a5b53566ef47da7580f22a112e6dba5ee139
Rizwan <rizwanmushtaq15@gmail.com>
2025-09-24 17:06:38 +0200

    docs: at StringJoin.java

--- src/main/java/com/rizwanmushtaq/strings/StringJoin.java
+++ src/main/java/com/rizwanmushtaq/strings/StringJoin.java
@@ -14,4 +14,17 @@ public class StringJoin {
 
     return result.toString();
   }
+
+  /**
+   * Java 8+ feature
+   * String[] words = {"hello", "world"};
+   * String result = String.join(" ", words);
+   * System.out.println(result); // hello world
+   *
+   * Using Streams
+   * String[] words = {"hello", "world"};
+   * String result = Arrays.stream(words)
+   *                       .collect(Collectors.joining(" "));
+   * System.out.println(result); // hello world
+   */
 }
