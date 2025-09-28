70331bcd6c4ab5284bccb2d4a462740755900bbe
Rizwan <rizwanmushtaq15@gmail.com>
2025-09-28 05:32:52 +0200

    refactor: thread app code

--- src/main/java/com/rizwanmushtaq/networkingAndThreads/multithreadApp/MultiThreadApp01.java
+++ src/main/java/com/rizwanmushtaq/networkingAndThreads/multithreadApp/MultiThreadApp01.java
@@ -2,12 +2,12 @@ package com.rizwanmushtaq.networkingAndThreads.multithreadApp;
 
 public class MultiThreadApp01 {
   public static void main(String[] args) {
+    Runnable myThreadJob = new MyThreadJob();
+
     /**
      * It is no loner recommended approach to use Threads
      */
-    Runnable myThreadJob = new MyThreadJob();
     Thread t = new Thread(myThreadJob);
-
     t.start();
 
     System.out.println(Thread.currentThread().getName() + " : in main");
--- src/main/java/com/rizwanmushtaq/networkingAndThreads/multithreadApp/MyThreadApp02.java
+++ src/main/java/com/rizwanmushtaq/networkingAndThreads/multithreadApp/MyThreadApp02.java
@@ -6,6 +6,7 @@ import java.util.concurrent.Executors;
 public class MyThreadApp02 {
   public static void main(String[] args) {
     Runnable myThreadJob = new MyThreadJob();
+    
     /**
      * A Better alternation to run Threads
      * Don't Manage the threads at all
